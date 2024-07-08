68689c1a2cd40ce6fa8cec6f24bd1a8344447b7a
saul <dlathf3210@naver.com>
2024-07-08 22:01:24 +0900

    0708

---
+++ chapter2/new_react/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+
+  // div 태그 class 속성은 ./App.css 속성을 따른다. 
+  return (
+    <div className="App"> 
+      <h1>음악 : 클래식</h1>
+    </div>
+  );
+}
+
+export default App;
